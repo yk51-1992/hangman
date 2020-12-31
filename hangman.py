e421eb575ee49cfd1c7c4255cf9695a83b0d8718
YukiKuranaga <yk51@kurahisashiyukinoMacBook-Pro.local>
2020-12-31 12:08:30 +0900

    my first commit

---
+++ hangman.py
@@ -0,0 +1,40 @@
+import random
+
+def hangman(word):
+    wrong = 0
+    stages = ["",
+            "_______      ",
+            "|            ",
+            "|      |     ",
+            "|      0     ",
+            "|     /|\    ",
+            "|     / \    ",
+            ]
+    rletters = list(word)
+    board =["_"] * len(word)
+    win = False
+    print("Welcome to Hangman!")
+    while wrong < len(stages) - 1:
+        print("\n")
+        msg = "Guess 1 character: "
+        char = input(msg)
+        if char in rletters:
+            cind = rletters.index(char)
+            board[cind] = char
+            rletters[cind] = "$"
+        else:
+            wrong += 1
+        print(" ".join(board))
+        e = wrong + 1
+        print("\n".join(stages[0:e]))
+        if "_" not in board:
+            print("You win!")
+            print(" ".join(board))
+            win = True
+            break
+    if not win:
+        print("\n".join(stages[0:wrong+1]))
+        print("You lose! The answer is {}".format(word))
+
+word_list = ["lion", "wolf", "turtle", "mouse", "beatle", "bird"]
+hangman(random.choice(word_list))
